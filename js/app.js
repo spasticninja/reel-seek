3c265149c4cfd1d62bca46409d66bd99e59286c9
Allison Betancourt <allisonbbetancourt@gmail.com>
2015-03-18 14:55:36 -0500

    fixed relative links part 2

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ angular.module('reel-seek', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider){
   $routeProvider.
     when('/', {
-      templateUrl: '/views/landing.html', //landing page or "home"
+      templateUrl: 'views/landing.html', //landing page or "home"
       controller: 'landingCtrl'
     }).
     when('/search', {
-      templateUrl: '/views/search.html', //search page
+      templateUrl: 'views/search.html', //search page
       controller: 'searchCtrl'
     }).
     when('/movie/:movieId', {
-      templateUrl: '/views/movie.html', //movie page where each movie has a unique ID (the IMDb ID)
+      templateUrl: 'views/movie.html', //movie page where each movie has a unique ID (the IMDb ID)
       controller: 'movieCtrl'
     }).
     otherwise({
